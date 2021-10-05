072a0f7c5be46cca56ad110d728b8693b5f60a14
Marius Kluften <mkluften93@gmail.com>
2021-10-05 16:04:05 +0200

    Got to 4.2.4

--- todo/lib/todolist/todolist.ex
+++ todo/lib/todolist/todolist.ex
@@ -1,15 +1,40 @@
 defmodule TodoList do
-  def new(), do: MultiDict.new()
+  defstruct auto_id: 1, entries: %{}
+  def new(), do: %TodoList{}
 
   def add_entry(todo_list, entry) do
-    MultiDict.add(
-      todo_list,
-      entry.date,
-      entry
-    )
+    entry = Map.put(entry, :id, todo_list.auto_id)
+
+    new_entries =
+      Map.put(
+        todo_list.entries,
+        todo_list.auto_id,
+        entry
+      )
+
+    %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
   end
 
   def entries(todo_list, date) do
-    MultiDict.get(todo_list, date)
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        new_entry = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    new_entries = Map.delete(todo_list.entries, entry_id)
+    %TodoList{todo_list | entries: new_entries}
   end
 end
